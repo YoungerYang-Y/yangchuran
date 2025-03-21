82bfed107e75223cb0263bce54f342f52bd175ef
YangYang <1059376437@qq.com>
2025-03-21 23:24:42 +0800

    build: migrate ESLint configuration from .eslintrc.json to eslint.config.js and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: false,
+  ignores: [
+    '.github/**',
+    '.vscode/**',
+  ],
+  rules: {
+    'style/quotes': ['error', 'double'],
+    'style/semi': ['error', 'always'],
+    'no-console': ['warn'],
+    'curly': ['error', 'multi-or-nest'],
+  },
+})
